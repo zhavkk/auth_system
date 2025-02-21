8693b5c9aa58f06c9e003cf575b9571720348d91
mishanya <zhavkk@gmail.com>
2025-02-21 17:48:36 +0300

    final

--- app/core/kafka_producer.py
+++ app/core/kafka_producer.py
@@ -1,6 +1,7 @@
 import json
 from kafka import KafkaProducer
-
+import time
+time.sleep(10)
 producer = KafkaProducer(
     bootstrap_servers=['localhost:9092'],
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
